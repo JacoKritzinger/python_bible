b7ff77b0306b818351a8409611294e9945bc15a8
jaco Kritzinger <jacokritzinger05@gmail.com>
2021-03-02 14:56:36 +0200

    first commit

---
+++ hello_world.py
@@ -0,0 +1,11 @@
+hello = "hello world!"
+
+print(hello)
+ 
+name = "Jaco"
+
+print(type(name))
+ 
+message = 'john said to me "i will se e you later "'
+
+ 
---
+++ hello_you.py
@@ -0,0 +1,23 @@
+#Ask user for name
+
+name = input("What is your name?: ")
+print(name)
+
+#Ask user for age
+
+age = input("What is your age ?: ")
+print(age)
+
+
+#Ask user for city
+
+city = input("What city do you live in?: ")
+
+
+#Ask user what they enjoy
+
+love = input("What do you love doing?: ")
+
+#Create output text
+
+#Pritn output to screen
